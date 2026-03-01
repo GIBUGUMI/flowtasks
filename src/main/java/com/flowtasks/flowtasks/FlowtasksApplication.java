a3acb92fa6a8da6d924f2d5809bca04c915b2acc
Mohamed El Aazioui <mohamed.elaazioui@gmail.com>
2026-03-01 14:22:09 +0100

    added initial architecture setup

---
+++ src/main/java/com/flowtasks/flowtasks/FlowtasksApplication.java
@@ -0,0 +1,13 @@
+package com.flowtasks.flowtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowtasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowtasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowtasks/flowtasks/FlowtasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowtasks.flowtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowtasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
